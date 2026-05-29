978c8da777d91da3a04d18016f163278556d6fb9
Nepht123 <mwambanephtalie12@gmail.com>
2026-05-29 21:28:23 +0200

    I have succesfully created all the tables in my database. I can now slowly begin to add the data into the tables

--- DATABASE/rate_mm_database.sql
+++ DATABASE/rate_mm_database.sql
@@ -30,7 +30,49 @@ CREATE TABLE users
 id INT PRIMARY KEY AUTO_INCREMENT,
 username VARCHAR(50) UNIQUE NOT NULL,
 student_email VARCHAR(50) UNIQUE NOT NULL,
-password VARCHAR(255) NOT NULL
+password VARCHAR(255) NOT NULL,
+role VARCHAR(20) DEFAULT 'ROLE_STUDENT'
 );
 
-SELECT * FROM users;
+
+CREATE TABLE courses
+(
+id INT PRIMARY KEY AUTO_INCREMENT,
+course_code VARCHAR(15) UNIQUE NOT NULL,
+course_name VARCHAR (127) NOT NULL,
+abbreviation VARCHAR(63) NOT NULL,
+duration_years INT NOT NULL,
+faculty_id INT NOT NULL,
+
+FOREIGN KEY (faculty_id) REFERENCES faculties(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE course_modules
+(
+course_id INT NOT NULL,
+    module_id INT NOT NULL,
+    year_of_study INT NOT NULL,    
+    semester VARCHAR(15) NOT NULL, 
+    
+    PRIMARY KEY (course_id, module_id),
+    
+    FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE,
+    FOREIGN KEY (module_id) REFERENCES modules(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE reviews
+(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    rating INT NOT NULL,
+    comment_text TEXT NOT NULL,
+    date_posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INT NOT NULL,
+    module_id INT NOT NULL,
+    
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+	FOREIGN KEY (module_id) REFERENCES modules(id) ON DELETE CASCADE
+);
+
+SELECT * FROM reviews;
