279e9c82e184f94f95ee030186eb15383be7a5ab
Nepht123 <mwambanephtalie12@gmail.com>
2026-07-11 17:07:02 +0200

    I was able to list all the programmes under a particular faculty, now I just need to dispaly all the info regarding a selected module. I porbably need to also make changes to the 'View Module' but and make it a fixed size but I will deal with those kind of changes before I start sorting out the login and user side of the web application

--- Backend & Frontend/Backend/app.py
+++ Backend & Frontend/Backend/app.py
@@ -61,10 +61,7 @@ def getfaculties():
             print("MySQL database connection securely closed.")
 
 
-
-
-
-def get_modules(faculty_id):
+def getfaculties_id(Faculty_Name):
     mydatabase = None
 
     try:
@@ -85,11 +82,61 @@ def get_modules(faculty_id):
             print(i)
         '''
 
-        get_query="SELECT * FROM modules WHERE faculty_code = %s"
-        mycursor.execute(get_query)
-        all_modules = mycursor.fetchall()
+        get_query="SELECT id FROM faculties where fac_name= %s"
+        mycursor.execute(get_query, (Faculty_Name,))
+        all_faculties_id = mycursor.fetchall()
 
-        return all_modules
+        return all_faculties_id
+
+
+
+    except Error as error:
+        print(f" Database Error encountered: {error}")
+        return None
+
+
+    finally:
+        # 4. ENVIRONMENT CLEANUP
+        if mydatabase and mydatabase.is_connected():
+            mycursor.close()
+            mydatabase.close()
+            print("MySQL database connection securely closed.")
+
+
+
+def get_programmes(faculty_id):
+    mydatabase = None
+
+    try:
+        mydatabase = mysql.connector.connect(
+            host='127.0.0.1', 
+            user='root', 
+            password='4492340',
+             database='rate_mm')
+        
+
+        print("Connecting to MySQL Database...")
+        mycursor = mydatabase.cursor(dictionary=True)
+
+
+        '''# Practice to ensure that it is connected and working
+        mycursor.execute("show tables")
+        for i in mycursor:
+            print(i)
+        '''
+
+        get_query="SELECT * FROM courses Where faculty_id= %s"
+        mycursor.execute(get_query,(faculty_id,))
+        all_programmes = mycursor.fetchall()
+
+        Programmes=[]
+
+        for prog in all_programmes:
+            Programmes.append(prog["course_name"])
+
+        return Programmes
+    
+
 
 
 
@@ -109,6 +156,22 @@ def get_modules(faculty_id):
 
 
 
+print(get_programmes(getfaculties_id("Natural Science")[0]["id"]))
+
+'''
+ProG=[]
+
+
+
+for programmes in prog:
+        ProG.append(programmes["course_name"])
+
+for programmes in ProG:
+        print(programmes)
+
+'''
+
+
 
 
 
@@ -125,7 +188,27 @@ def index():
 def Program():
     # Flask automatically grabs 'faculty' from the URL query string
     faculty_name = request.args.get('faculty')
-    # Your logic here...
-    return render_template('1_Program.html', faculty=faculty_name)
+
+    faculty_id=getfaculties_id(faculty_name)[0]["id"]
+
+    faculty_programmes=get_programmes(faculty_id)
+    return render_template('1_Program.html', faculty=faculty_name, programmes=faculty_programmes)
+
+
+@app.route("/Modules", methods=["GET"])
+def Program_modules():  # <--- url_for looks at THIS name right here!
+    module = request.args.get("module")
+
+    #Need to create a function that shows all the info regarding the particular module 
+    
+    return
+
+
+
+'''
+I need:
+    Faculty name
+    All programmes in faculty
+    '''
 
 
